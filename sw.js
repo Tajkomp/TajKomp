032029fec3bc897f59391f939dbf0ee34000bae6
Tajkomp <shoxrahimov@icloud.com>
2026-04-10 05:08:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
 self.addEventListener('fetch', function(event) {});
+// Обработка клика по уведомлению
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close(); // Закрываем уведомление после клика
+    event.waitUntil(
+        clients.openWindow('/') // Открываем главную страницу сайта TajKomp
+    );
+});
